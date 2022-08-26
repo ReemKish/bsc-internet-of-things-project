c14e02a6c31d8eeddd0f0ac7d4c76403649f0481
Re'em Kishinevsky <reem.kishinevsky@gmail.com>
2022-08-26 21:20:26 +0300

    Notification service functions now return Future<bool> to allow for async computation (useful e.g. for displaying progress huds during API calls)

--- app/lib/services/notification_service.dart
+++ app/lib/services/notification_service.dart
@@ -12,32 +12,25 @@ class NotificationService {
   static const namespace = "arc-notification-hub-namespace";
   static const sasToken = "SharedAccessSignature sr=https%3A%2F%2Farc-notification-hub-namespace.servicebus.windows.net%2Farc-notification-hub&sig=GWKH1XbiCn5qFOm6DSk3UHHc%2BHCcORZ53tlGBTJGCcE%3D&se=11661451928&skn=DefaultFullSharedAccessSignature";
 
-  static void subscribe(String email) async {
-    FirebaseMessaging.instance.getToken().then((value) {
-      String token = value!;
-      email = email.replaceAll('@', '_at_');
-      _addInstallation(email, token).then((http.Response value) {
-        debugPrint(value.statusCode == 200 ?
-          "subscribe($email) - failed with status ${value.statusCode}" :
-          "subscribe($email) - success"
-        );
-      });
-    });
+  static Future<bool> subscribe(String email) async {
+    email = email.replaceAll('@', '_at_');
+    String? gcmToken = await FirebaseMessaging.instance.getToken();
+    final statusCode = (await _addInstallation(email, gcmToken!)).statusCode;
+    debugPrint(statusCode == 200 ?
+      "subscribe($email) - success" :
+      "subscribe($email) - failed with status $statusCode");
+    return statusCode == 200;
   }
 
-  static void followDevice(String email, String deviceId) async {
-    FirebaseMessaging.instance.getToken().then((value) {
-      String token = value!;
-      email = email.replaceAll('@', '_at_');
-      _patchInstallation(email, token, deviceId).then((http.Response value) {
-        debugPrint(value.statusCode == 200 ?
-          "subscribe($email) - failed with status ${value.statusCode}" :
-          "subscribe($email) - success"
-        );
-      });
-    });
+  static Future<bool> followDevice(String email, String deviceId) async {
+    email = email.replaceAll('@', '_at_');
+    String? gcmToken = await FirebaseMessaging.instance.getToken();
+    final statusCode = (await _patchInstallation(email, gcmToken!, deviceId)).statusCode;
+    debugPrint(statusCode == 200 ?
+      "followDevice($email, $deviceId) - success" :
+      "followDevice($email, $deviceId) - failed with status $statusCode");
+    return statusCode == 200;
   }
-
 
   static void registerForegroundHandler(Function(String deviceId) action) {
     FirebaseMessaging.onMessage.listen((RemoteMessage message) {
@@ -51,29 +44,23 @@ class NotificationService {
     });
   }
 
-  static Future<http.Response> _addInstallation(String id, String gcmToken) async {
-    final headers = {"Content-Type": "application/json", "Authorization": sasToken};
-    return http.put(
-      Uri.parse("https://$namespace.servicebus.windows.net/$name/installations/$id?api-version=2015-01"),
-      headers: headers,
-      body: jsonEncode(<String, dynamic>{
-        "installationId": id,
-        "platform": "gcm",
-        "pushChannel": gcmToken,
-      })
-    );
-  }
+  static Future<http.Response> _addInstallation(String id, String gcmToken) async => http.put(
+    Uri.parse("https://$namespace.servicebus.windows.net/$name/installations/$id?api-version=2015-01"),
+    headers: {"Content-Type": "application/json", "Authorization": sasToken},
+    body: jsonEncode(<String, dynamic>{
+      "installationId": id,
+      "platform": "gcm",
+      "pushChannel": gcmToken,
+    })
+  );
 
-  static Future<http.Response> _patchInstallation(String id, String gcmToken, String tag) async {
-    final headers = {"Content-Type": "application/json-patch+json", "Authorization": sasToken};
-    return http.patch(
-      Uri.parse("https://$namespace.servicebus.windows.net/$name/installations/$id?api-version=2015-01"),
-      headers: headers,
-      body: jsonEncode([{
-        "op": "add",
-        "path": "/tags",
-        "value": tag,
-      }])
-    );
-  }
+  static Future<http.Response> _patchInstallation(String id, String gcmToken, String tag) async => http.patch(
+    Uri.parse("https://$namespace.servicebus.windows.net/$name/installations/$id?api-version=2015-01"),
+    headers: {"Content-Type": "application/json-patch+json", "Authorization": sasToken},
+    body: jsonEncode([{
+      "op": "add",
+      "path": "/tags",
+      "value": tag,
+    }])
+  );
 }
